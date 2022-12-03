d2a220a9fb3959fe24468a47084359afd5877e5f
lesha <lesha.rudiko@gmail.com>
2022-12-03 19:01:19 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+
+//  var c;
+//     for (let x=10; x<400; x+=10) {
+//         for(let y=10; y<400; y+=20) {
+//             c = Shape.Circle(x,y,5);
+//             c.fillColor = 'green';
+//         }
+//     }
+
+
+$(document).ready (function() {
+    'use strict';
+    paper.install(window);
+    paper.setup(document.getElementById('mainCanvas'));
+     var tool = new Tool ();
+        var c = Shape.Circle (200, 200, 150);
+        c.fillColor = 'green';
+        var text = new PointText (200, 200); 
+        text.justification = 'center'; 
+        text.fillColor = 'black'
+        text.fontSize = 50;
+        text.content = 'hello world';
+
+     tool.onMouseDown = function(event) {
+        var c = Shape.Circle(event.point, 5);
+        c.fillColor = 'white';
+     };
+    paper.view.draw();
+});
